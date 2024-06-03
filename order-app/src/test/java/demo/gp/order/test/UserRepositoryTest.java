db09d6b11cebe253c1358781ecc31cc5e2732e1a
doukai <doukai.java@gmail.com>
2024-06-03 14:43:13 +0800

    update

---
+++ order-app/src/test/java/demo/gp/order/test/UserRepositoryTest.java
@@ -0,0 +1,27 @@
+package demo.gp.order.test;
+
+import demo.gp.order.dto.enumType.UserType;
+import demo.gp.order.dto.objectType.User;
+import demo.gp.order.repository.UserRepository;
+import io.nozdormu.spi.context.BeanContext;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(TestResultLoggerExtension.class)
+public class UserRepositoryTest {
+
+    private final UserRepository userRepository = BeanContext.get(UserRepository.class);
+
+    @Test
+    void queryVIPUserListTest() {
+        List<User> userLit = userRepository.queryVIPUserList().block();
+        Assertions.assertAll(
+                userLit.stream().map((item) -> () -> assertEquals(item.getUserType(), UserType.VIP))
+        );
+    }
+}
---
+++ order-package/src/main/java/demo/gp/order/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package demo.gp.order.repository;
+
+import demo.gp.order.dto.annotation.Query;
+import demo.gp.order.dto.annotation.UserListQueryArguments;
+import demo.gp.order.dto.annotation.UserTypeExpression1;
+import demo.gp.order.dto.enumType.UserType;
+import demo.gp.order.dto.objectType.User;
+import io.graphoenix.core.dto.enumType.Operator;
+import io.graphoenix.spi.annotation.GraphQLOperation;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@GraphQLOperation
+public interface UserRepository {
+
+    @Query(userList = @UserListQueryArguments(userType = @UserTypeExpression1(opr = Operator.EQ, val = UserType.VIP)))
+    Mono<List<User>> queryVIPUserList();
+}
