38963dd88977058afe7acfe0ed49805bb234ff19
doukai <doukai.java@gmail.com>
2024-06-04 17:09:59 +0800

    update

--- order-app/src/test/java/demo/gp/order/test/UserRepositoryTest.java
+++ order-app/src/test/java/demo/gp/order/test/UserRepositoryTest.java
@@ -1,13 +1,14 @@
 package demo.gp.order.test;
 
 import demo.gp.order.dto.enumType.UserType;
+import demo.gp.order.dto.inputObjectType.UserInput;
 import demo.gp.order.dto.objectType.User;
 import demo.gp.order.repository.UserRepository;
 import io.nozdormu.spi.context.BeanContext;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,16 +21,72 @@ public class UserRepositoryTest {
     @Test
     void queryVIPUserListTest() {
         List<User> userLit = userRepository.queryVIPUserList().block();
-        Assertions.assertAll(
+        assertAll(
                 userLit.stream().map((item) -> () -> assertEquals(item.getUserType(), UserType.VIP))
         );
     }
 
     @Test
     void queryVIPUserListByUserTypeTest() {
-        List<User> userLit = userRepository.queryVIPUserListByUserType(UserType.REGULAR).block();
-        Assertions.assertAll(
+        List<User> userLit = userRepository.queryUserListByUserType(UserType.REGULAR).block();
+        assertAll(
                 userLit.stream().map((item) -> () -> assertEquals(item.getUserType(), UserType.REGULAR))
+        );
+    }
+
+    @Test
+    void queryUserNameListByUserTypeTest() {
+        List<User> userLit = userRepository.queryUserNameListByUserType(UserType.REGULAR).block();
+        assertAll(
+                userLit.stream().map((item) ->
+                        () -> assertAll(
+                                () -> assertNotNull(item.getName()),
+                                () -> assertNull(item.getId()),
+                                () -> assertNull(item.getUserType()),
+                                () -> assertNull(item.getEmail()),
+                                () -> assertNull(item.getPhoneNumbers())
+                        )
+                )
+        );
+    }
+
+    @Test
+    void queryUserOrdersListByNameTest() {
+        User user = userRepository.queryUserOrdersListByName("Alice").block();
+        assertAll(
+                () -> assertEquals(user.getOrders().size(), 1),
+                () -> assertEquals(new ArrayList<>(user.getOrders()).get(0).getItems().size(), 2),
+                () -> assertEquals(new ArrayList<>(new ArrayList<>(user.getOrders()).get(0).getItems()).get(0).getProduct().getName(), "Laptop"),
+                () -> assertEquals(new ArrayList<>(new ArrayList<>(user.getOrders()).get(0).getItems()).get(0).getQuantity(), 1),
+                () -> assertEquals(new ArrayList<>(new ArrayList<>(user.getOrders()).get(0).getItems()).get(1).getProduct().getName(), "Tablet"),
+                () -> assertEquals(new ArrayList<>(new ArrayList<>(user.getOrders()).get(0).getItems()).get(1).getQuantity(), 2)
+        );
+    }
+
+    @Test
+    void queryUserCountByUserTypeTest() {
+        List<User> userList = userRepository.queryUserCountByUserType().block();
+        assertAll(
+                () -> assertEquals(userList.size(), 2),
+                () -> assertEquals(new ArrayList<>(userList).get(0).getUserType(), UserType.VIP),
+                () -> assertEquals(new ArrayList<>(userList).get(0).getIdCount(), 12),
+                () -> assertEquals(new ArrayList<>(userList).get(1).getUserType(), UserType.REGULAR),
+                () -> assertEquals(new ArrayList<>(userList).get(1).getIdCount(), 12)
+        );
+    }
+
+    @Test
+    void mutationUserTest() {
+        UserInput userInput = new UserInput();
+        userInput.setName("Yara");
+        userInput.setEmail("yara@example.com");
+        userInput.setUserType(UserType.VIP);
+        User user = userRepository.mutationUser(userInput).block();
+        assertAll(
+                () -> assertNotNull(user.getId()),
+                () -> assertEquals(user.getName(), "Yara"),
+                () -> assertEquals(user.getEmail(), "yara@example.com"),
+                () -> assertEquals(user.getUserType(), UserType.VIP)
         );
     }
 }
--- order-package/src/main/java/demo/gp/order/repository/UserRepository.java
+++ order-package/src/main/java/demo/gp/order/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package demo.gp.order.repository;
 
-import demo.gp.order.dto.annotation.Query;
-import demo.gp.order.dto.annotation.UserListQueryArguments;
-import demo.gp.order.dto.annotation.UserTypeExpression1;
+import demo.gp.order.dto.annotation.*;
 import demo.gp.order.dto.enumType.UserType;
+import demo.gp.order.dto.inputObjectType.UserInput;
 import demo.gp.order.dto.objectType.User;
+import io.graphoenix.core.dto.annotation.StringExpression1;
 import io.graphoenix.core.dto.enumType.Operator;
 import io.graphoenix.spi.annotation.GraphQLOperation;
+import io.graphoenix.spi.annotation.SelectionSet;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -18,5 +19,21 @@ public interface UserRepository {
     Mono<List<User>> queryVIPUserList();
 
     @Query(userList = @UserListQueryArguments(userType = @UserTypeExpression1(opr = Operator.EQ, $val = "userType")))
-    Mono<List<User>> queryVIPUserListByUserType(UserType userType);
+    Mono<List<User>> queryUserListByUserType(UserType userType);
+
+    @Query(userList = @UserListQueryArguments(userType = @UserTypeExpression1(opr = Operator.EQ, $val = "userType")))
+    @SelectionSet("{ name }")
+    Mono<List<User>> queryUserNameListByUserType(UserType userType);
+
+    @Query(user = @UserQueryArguments(name = @StringExpression1(opr = Operator.EQ, $val = "name")))
+    @SelectionSet("{ name orders { items { product { name } quantity } } }")
+    Mono<User> queryUserOrdersListByName(String name);
+
+    @Query(userList = @UserListQueryArguments(groupBy = {"userType"}))
+    @SelectionSet("{ userType idCount }")
+    Mono<List<User>> queryUserCountByUserType();
+
+    @Mutation(user = @UserMutationArguments($input = "userInput"))
+    @SelectionSet("{ id name email userType }")
+    Mono<User> mutationUser(UserInput userInput);
 }
